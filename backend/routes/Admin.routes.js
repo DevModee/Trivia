86f16a4c8b44ca263bbeff8c1f827f8d4db098cd
DevModee <caelien2.0@gmail.com>
2024-11-26 20:30:19 -0300

    added routes

--- backend/routes/Admin.routes.js
+++ backend/routes/Admin.routes.js
@@ -1,7 +1,11 @@
 import express from 'express';
-import { registerAdmin } from '../controllers/admin.controller.js';
+import { registerAdmin, getAdmins, deleteAdmins } from '../controllers/admin.controller.js';
 
 const router = express.Router();
+
 router.post('/register', registerAdmin);
+router.get('/admins', getAdmins);
+router.delete('/admins/:id', deleteAdmin);
+
 
 export default router; 
