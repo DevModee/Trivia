e0ab8216c28b125db0261a2605ac006a0bef64d5
DevModee <caelien2.0@gmail.com>
2024-11-26 20:32:16 -0300

    import edited

--- backend/routes/Admin.routes.js
+++ backend/routes/Admin.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { registerAdmin, getAdmins, deleteAdmins } from '../controllers/admin.controller.js';
+import { registerAdmin, getAdmins, deleteAdmin } from '../controllers/admin.controller.js';
 
 const router = express.Router();
 
