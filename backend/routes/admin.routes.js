e7f232c4611b655eb1d6ad69e0e5aeefd957bc60
DevModee <caelien2.0@gmail.com>
2024-11-28 13:36:57 -0300

    bcrypt installed, hashedPassword  and loginAdmin controller

--- backend/routes/admin.routes.js
+++ backend/routes/admin.routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { registerAdmin, getAdmins, deleteAdmin } from '../controllers/admin.controller.js';
+import { registerAdmin, getAdmins, deleteAdmin, loginAdmin } from '../controllers/admin.controller.js';
 
 const router = express.Router();
 
 router.post('/admins/register', registerAdmin);
+router.post('/login', loginAdmin);
 router.get('/admins', getAdmins);
 router.delete('/admins/:id', deleteAdmin);
 
