67c779b544e978884fdcfffcdbb88d99671acaff
DevModee <caelien2.0@gmail.com>
2024-11-28 13:20:10 -0300

    edited timestamps

--- backend/models/trivia.model.js
+++ backend/models/trivia.model.js
@@ -3,31 +3,31 @@ import mongoose from 'mongoose';
 const { Schema, model } = mongoose;
 
 const questionSchema = new Schema({
-    text: {
-        type: String,
-        required: true,
-    },
-    value: {
-        type: Boolean,
-        required: true,
-    },
-}, {
-    timestamps: true
+  text: {
+    type: String,
+    required: true,
+  },
+  value: {
+    type: Boolean,
+    required: true,
+  },
 });
 
 const triviaSchema = new Schema({
-    title: {
-        type: String,
-        required: true,
-    },
-    activaHasta: {
-        type: Date,
-        required: true,
-    },
-    description: {
-        type: String,
-    },
-    preguntas: [questionSchema],
+  title: {
+    type: String,
+    required: true,
+  },
+  activaHasta: {
+    type: Date,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  preguntas: [questionSchema],
+}, {
+  timestamps: true
 });
 
 const Trivia = model('Trivia', triviaSchema);
