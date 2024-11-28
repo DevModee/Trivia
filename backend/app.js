9dff2d2badf8cc1da10530a5909f84b9da6bf9af
DevModee <caelien2.0@gmail.com>
2024-11-28 16:23:25 -0300

    added trivia routes in app.js

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { config } from 'dotenv';
 import connectDB from './config/db.js';
 import adminRoutes from "./routes/admin.routes.js"
+import triviaRoutes from "./routes/trivia.routes.js"
 
 config();
 connectDB();
@@ -10,6 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use('/api', adminRoutes);
+app.use('/api', triviaRoutes)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
