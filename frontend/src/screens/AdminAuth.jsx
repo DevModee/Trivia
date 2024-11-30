8e62df6db4c3e58316cb4dc5c6115292f95db2d3
DevModee <caelien2.0@gmail.com>
2024-11-30 00:40:47 -0300

    added LoginForm

--- frontend/src/screens/AdminAuth.jsx
+++ frontend/src/screens/AdminAuth.jsx
@@ -1,9 +1,9 @@
-import RegisterForm from "../components/RegisterForm"
+import LoginForm from "../components/LoginForm"
 
 const AdminAuth = () => {
   return (
     <div className="flex flex-1 bg-stone-800 h-screen items-center px-[40%]">
-      <RegisterForm />
+      <LoginForm />
     </div>
   )
 }
