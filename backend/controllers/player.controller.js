d5c46009ff342ac7cd853b860a544bf8636305a6
DevModee <caelien2.0@gmail.com>
2024-12-03 00:42:11 -0300

    renamed user

--- backend/controllers/player.controller.js
+++ backend/controllers/player.controller.js
@@ -62,7 +62,7 @@ export const getPlayerById = async (req, res) => {
         if (!player) {
             return res.status(404).json({ message: "User not found" });
         }
-        res.status(200).json(user);
+        res.status(200).json(player);
     } catch (error) {
         res.status(500).json({ message: "Error fetching user", error });
     }
