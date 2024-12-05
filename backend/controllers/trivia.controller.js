a1698356646f63308bbf8a75654cd320bf7f030c
DevModee <caelien2.0@gmail.com>
2024-12-05 15:06:53 -0300

    updateTrivia and deleteTrivia, routes added

--- backend/controllers/trivia.controller.js
+++ backend/controllers/trivia.controller.js
@@ -47,5 +47,42 @@ export const getTriviaById = async (req, res) => {
   }
 };
 
+export const updateTrivia = async (req, res) => {
+  const { trivia_id } = req.params;
+  const { title, description, preguntas, activaHasta } = req.body;
 
+  try {
+    const trivia = await Trivia.findById(trivia_id);
+    if (!trivia) {
+      return res.status(404).json({ message: "Trivia not found" });
+    }
+
+    if (title) trivia.title = title;
+    if (description) trivia.description = description;
+    if (preguntas) trivia.preguntas = preguntas;
+    if (activaHasta) trivia.activaHasta = activaHasta;
+
+    await trivia.save();
+
+    res.status(200).json({ message: "Trivia updated successfully", trivia });
+  } catch (error) {
+    res.status(500).json({ message: "Error updating trivia", error });
+  }
+};
+
+// Eliminar Trivia
+export const deleteTrivia = async (req, res) => {
+  const { trivia_id } = req.params;
+
+  try {
+    const trivia = await Trivia.findByIdAndDelete(trivia_id);
+    if (!trivia) {
+      return res.status(404).json({ message: "Trivia not found" });
+    }
+
+    res.status(200).json({ message: "Trivia deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Error deleting trivia", error });
+  }
+};
 
